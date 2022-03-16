e3bdccd805c4994ffa8c45a97375e809f2ca3616
root <thelmachepkoech@gmail.com>
2022-03-15 22:57:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
 /**
- * main - Putchar
+ * main - entry point
  *
- * _putchar print the characters given
- * Return: Always 0 (Success)
+ * Return:Always 0 (Success)
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+	}
 	return (0);
 }
