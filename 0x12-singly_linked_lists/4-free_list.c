c687ff57148020110d032001f5f520625a0e7693
root <thelmachepkoech@gmail.com>
2022-04-22 00:28:56 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: Pointer to the actual position of the linked list (struct)
+ *
+ * Return: Size of the linked_list.
+ *
+ **/
+
+void free_list(list_t *head)
+{
+	list_t *actual_node;
+	list_t *next_node;
+
+	actual_node = head;
+
+	while (actual_node != NULL)
+	{
+		next_node = actual_node->next;
+		free(actual_node->str);
+		free(actual_node);
+
+		actual_node = next_node;
+	}
+}
