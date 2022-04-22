985144d2f8773cdb8bb93fc6e4b90b77248b8350
root <thelmachepkoech@gmail.com>
2022-04-22 00:34:33 -0700

    list.h

---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,30 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+
+#include <stdio.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (mallc'd string)
+ * @len: length of string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ **/
+
+typedef struct list_s
+{
+	char *str;
+
+	unsigned int len;
+
+	struct list_s *next;
+} list_t;
+
+int _putchar(char);
+size_t print_list(const list_t *);
+list_t *add_node(list_t **, const char *);
+size_t list_len(const list_t *);
+list_t *add_node_end(list_t **, const char *);
+void free_list(list_t *head);
+#endif /*LISTS*/
